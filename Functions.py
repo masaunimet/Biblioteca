b6cb0527e46f37c12ad46dab095cc74f2328b5c2
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2022-03-28 17:27:48 -0400

    funcion leer y escribir objeto lista

--- Functions.py
+++ Functions.py
@@ -1,3 +1,8 @@
+from copyreg import pickle
+
+
+import pickle
+
 #Función hash, toma el key, lo convierte en su equivalente ascii y toma el residuo de la división entre 2
 
 def hash_function(string):
@@ -7,4 +12,15 @@ def hash_function(string):
     hash_value = hash_value%2
 
     return hash_value
+
+#Funcion para ecribir la lista en el archivo
+
+def write_on_file(list):
+    with open('database.txt','wb') as f:
+        pickle.dump(list,f)
+
+def read_file():
+    with open('database.txt', 'rb') as f:
+        lista = pickle.load(f)
+        return lista
         
